cbf1106f8e68bfce32bb9da095c235c46fbf90cb
Felix Watson <felix.dork@gmail.com>
2023-07-18 16:35:26 +1000

    fixing app

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-import { Box } from "@chakra-ui/react"
-import NavBar from "./components/navbar"
-import Landing from "./components/landing"
-import Program from "./components/program"
-import Timetable from "./components/timetable"
+import { Box } from "@chakra-ui/react";
+import NavBar from "./components/navbar";
+import Landing from "./components/landing";
+import Program from "./components/program";
+import Timetable from "./components/timetable";
 
 function App() {
   return (
     <Box>
       <NavBar />
-      <div as='main' mt='50'>
       <Landing />
       <Program />
-      <Timetable />
-      </div>
     </Box>
-  )
+  );
 }
 
-export default App
+export default App;
