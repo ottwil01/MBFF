22f803a0f467f971d830d05b673f63a83d90900b
Felix Watson <felix.dork@gmail.com>
2023-07-19 11:49:11 +1000

    added loading to submit and removed coloring ready for theme file

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -14,12 +14,15 @@ import emailjs from "@emailjs/browser";
 function ContactForm() {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
 
   function handleInput(target, event) {
     target(event.target.value);
   }
 
   function handleSubmit() {
+    setLoading(true);
+
     const params = {
       to_name: "mbffcontact@gmail.com",
       from_name: email,
@@ -31,9 +34,11 @@ function ContactForm() {
       .then(
         (result) => {
           console.log(result.status, result.text);
+          setLoading(false);
         },
         (error) => {
           console.log(error.status, error.text);
+          setLoading(false);
         }
       );
 
@@ -44,7 +49,7 @@ function ContactForm() {
   return (
     <Flex
       w={"full"}
-      h={"100vh"}
+      minH={"100vh"}
       alignItems={"center"}
       justifyContent={"center"}
       direction={"column"}
@@ -54,34 +59,34 @@ function ContactForm() {
         Contact
       </Text>
       <FormControl
-        color="white"
+        color="form.color"
         display={"flex"}
         flexDirection={"column"}
-        w="55%"
+        w={["90%", "90%", "55%"]}
       >
         <FormLabel>Email Address</FormLabel>
         <Input
           type="email"
-          bg="blue.600"
+          bg="form.blue"
           placeholder="From..."
           w="66%"
           size={"lg"}
           fontSize={"3xl"}
           fontWeight={"bold"}
-          _placeholder={{ color: "gray.400" }}
+          _placeholder={{ color: "form.placeholder" }}
           value={email}
           onInput={(e) => handleInput(setEmail, e)}
         />
         <FormLabel>Message</FormLabel>
         <Textarea
           type="text"
-          bg="blue.600"
+          bg="form.blue"
           placeholder="Type message here..."
           h={64}
           resize={"vertical"}
           fontSize={"3xl"}
           fontWeight={"bold"}
-          _placeholder={{ color: "gray.400" }}
+          _placeholder={{ color: "form.placeholder" }}
           value={message}
           onInput={(e) => handleInput(setMessage, e)}
           mb={6}
@@ -91,6 +96,7 @@ function ContactForm() {
           w="max-content"
           fontSize={"3xl"}
           padding={8}
+          isLoading={loading}
         >
           Submit!
         </Button>
