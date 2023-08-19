de88ed1b46b370689faead3fd567ee1b048a2b44
William Otto <ottwil01@gmail.com>
2023-08-19 19:09:49 +1000

    program image

--- src/components/program.jsx
+++ src/components/program.jsx
@@ -123,6 +123,7 @@ function ProgramComponent(props) {
               alt="thumbnail"
               objectFit={"cover"}
               boxSize={"fit-content"}
+              h="100%"
           />
           </Flex>
         </Flex>
